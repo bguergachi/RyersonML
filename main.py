02f73b04d29a3d272f3e0d265a101501bd50d591
Adam <bguergachi@gmail.com>
2017-03-16 16:24:16 -0400

    Randomized data for better training and testing Added option to test output with specific inputs Changed model directory to inside the project folder

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
 import tensorflow as tf
 import numpy as np
 import pandas as pd
@@ -12,9 +8,9 @@ DIABETES_TEST = "C://users/ONTARIO/PycharmProjects/DATA/DIABETES_test.csv"
 
 # Load datasets.
 training_set = tf.contrib.learn.datasets.base.load_csv_without_header (filename=DIABETES_TRAINING,
-                                                       target_dtype=np.float32, features_dtype=np.float32)
+                                                       target_dtype=np.int, features_dtype=np.float32)
 test_set = tf.contrib.learn.datasets.base.load_csv_without_header(filename=DIABETES_TEST,
-                                                   target_dtype=np.float32, features_dtype=np.float32)
+                                                   target_dtype=np.int, features_dtype=np.float32)
 
 #feature columns
 feature_columns = [tf.contrib.layers.real_valued_column("", dimension=4)]
@@ -22,12 +18,13 @@ feature_columns = [tf.contrib.layers.real_valued_column("", dimension=4)]
 # Build 3 layer DNN with 10, 20, 10 units respectively.
 classifier = tf.contrib.learn.DNNClassifier(hidden_units=[10, 20, 10],
                                             n_classes=2,
-                                            model_dir="/tmp/DIABETES_model",feature_columns=feature_columns)
-
+                                            model_dir="diabetes_model",feature_columns=feature_columns)
+print('Training Neural Network....')
 # Fit model.
 classifier.fit(x=training_set.data,
                y=training_set.target,
                steps=10000)
+print('Training completed!')
 
 # Evaluate accuracy.
 accuracy_score = classifier.evaluate(x=test_set.data,
@@ -35,11 +32,12 @@ accuracy_score = classifier.evaluate(x=test_set.data,
 print('Accuracy: {0:f}'.format(accuracy_score))
 
 
-# Classify two new DIABETES tumor samples.
-'''
+# Classify two new patient samples.
 new_samples = np.array(
-    [], dtype=float)
-y = classifier.predict(new_samples)
-for i in y:
-    print (i)
-'''
+    [[1.78,2.4,30.4,5]], dtype=float)
+y = classifier.predict_proba(new_samples,as_iterable=False)
+
+print('Inputs: {}'.format(new_samples))
+print ('Predictions: {}'.format(y))
+
+
